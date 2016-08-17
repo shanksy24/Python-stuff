359960a6f925091d244af537035ba1f130ed4968
shanksy24 <shanksy.lakers@gmail.com>
2016-08-17 14:15:32 +0930

    Update vonkoch_snowflake_fraction.py

--- vonkoch_snowflake_fraction.py
+++ vonkoch_snowflake_fraction.py
@@ -1,17 +1,27 @@
 #Gives perimeter and area for vonkoche snowflake at "n" stage
 
 user_input = int(raw_input("Stage num : "))
-area_tally = 1
+area_tally = [1,1]
+area_total = [1,1]
 
 if user_input == 0:
 	print "Perimeter : 3 units"
 	print "Area      : 1 unit squared"
 	exit()
 
-for n in range(1, user_input+1):
-	area_tally += (1/float(9**n)) * 3 * float(4**(int(n-1)))
-  
-print "Area      : " + str(area_tally) + " units squared"
+area_tally[0] *= 3 
+area_tally[1] *= 9
+print area_tally
+print area_total
+
+for n in range(1, user_input):
+	area_tally[0] *= 4
+	area_total[0] += area_tally[0]
+	area_tally[1] *= 9
+	area_total[1] += area_tally[1]
+	print area_total
+
+print "Area      : " + str(area_total[0]) + "/" + str(area_total[1]) + " units squared"
 
 perimeter = 3 * (float(float(4)/float(3))**user_input)
 
