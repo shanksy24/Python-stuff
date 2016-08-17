c08f7b2486d25d25d96887f123e9c455789c639e
shanksy24 <shanksy.lakers@gmail.com>
2016-08-17 18:06:15 +0930

    Update vonkoch_snowflake_fraction.py

--- vonkoch_snowflake_fraction.py
+++ vonkoch_snowflake_fraction.py
@@ -2,27 +2,33 @@
 
 user_input = int(raw_input("Stage num : "))
 area_tally = [1,1]
-area_total = [1,1]
+new_fractn = [9,9]
+perimeter = [3,1]
 
 if user_input == 0:
 	print "Perimeter : 3 units"
 	print "Area      : 1 unit squared"
 	exit()
 
-area_tally[0] *= 3 
-area_tally[1] *= 9
 print area_tally
-print area_total
 
-for n in range(1, user_input):
-	area_tally[0] *= 4
-	area_total[0] += area_tally[0]
-	area_tally[1] *= 9
-	area_total[1] += area_tally[1]
-	print area_total
+for n in range(1, user_input+1):
+	if n == 1:
+		area_tally = [4,3]
+		
+	else:
+		area_tally[0] *= 9
+		area_tally[1] *= 9
+		area_tally[0] += (4**(n-1))
 
-print "Area      : " + str(area_total[0]) + "/" + str(area_total[1]) + " units squared"
+print "Area      : " + str(area_tally[0]) + "/" + str(area_tally[1]) + " units squared"
 
-perimeter = 3 * (float(float(4)/float(3))**user_input)
+for n in range(user_input):
+	perimeter[0] *= 4
+	perimeter[1] *= 3
 
-print "Perimeter : " + str(perimeter) + " units"
+perimeter[0] /= 3
+perimeter[1] /= 3
+
+
+print "Perimeter : " + str(perimeter[0]) + "/" + str(perimeter[1]) + " units"
