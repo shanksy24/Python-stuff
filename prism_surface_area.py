2a12c4556e7a37a70e577ae292d8f75c09b5b333
shanksy24 <shanksy.lakers@gmail.com>
2016-09-01 21:22:09 +0930

    Update prism_surface_area.py

--- prism_surface_area.py
+++ prism_surface_area.py
@@ -7,5 +7,3 @@ h = raw_input("Height : ")
 sa = 2.0*float(n)*((float(s)*((float(s)/2.0)/tan(radians(180.0/float(n)))))/2.0)+(float(s)*float(h)*float(n))
 
 print "\nSurface Area = ", str(sa), "units squared"
-
-print tan(radians(9))
